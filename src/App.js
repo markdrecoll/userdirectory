e6c9044b601beaedb08ac4d70d7a884831f8d04e
mdre797 <markdrecoll@gmail.com>
2021-06-03 17:49:14 -0500

    Added css styling. Adjusted Table.

--- src/App.js
+++ src/App.js
@@ -6,20 +6,13 @@ import API from './utils/API'
 function App() {
 
   // create a state for user data
-  const[userData, setUserData] = useState([
-    // {
-    // name: "",}
-  ]);
+  const[userData, setUserData] = useState([]);
 
   // create a state for inputted user data, initialize as empty string
   const[userInputData, setUserInputData] = useState("");
 
   // create a state for the filter
-  const[userFilteredData, setUserFilteredData] = useState([
-  //   {
-  //   name: ""
-  // }
-]);
+  const[userFilteredData, setUserFilteredData] = useState([]);
   
   // on page load run api call to get user data
   useEffect( () => {
@@ -39,43 +32,88 @@ function App() {
     setUserFilteredData(tempFilter);
   }
 
-  const sortAlpha = () => {
+  // sorts each user based on what is being compared
+  const sortAlpha = (event) => {
 
-    function compare( a, b ) {
-      if ( a.email < b.email ){
-        return -1;
-      }
-      if ( a.email > b.email ){
-        return 1;
-      }
-      return 0;
+    // first name, last name, and age have prefixes in their data, name and dob respectively
+    let prefixToUserData = "name";
+
+    // the button events pass in the id name
+    let typeOfUserData = event.target.id;
+    
+    // the switch cases change it to dob, or blank, the default is set as name above
+    switch(typeOfUserData){
+      case "email":
+        prefixToUserData = "";
+        break;
+      case "phone":
+        prefixToUserData = "";
+        break;
+      case "age":
+        prefixToUserData = "dob";
+        break;      
     }
 
-    // const oldState = userFilteredData
+    // if one user's attribute is alaphabetically before or after, sort accordingly
+    function compare( a, b ) {
+
+      // if the prefis is name or dob that is passed in before the event.target.id
+      if(prefixToUserData==="name" || prefixToUserData==="dob"){
+        if ( a[prefixToUserData][event.target.id] < b[prefixToUserData][event.target.id] ){return -1;}
+        if ( a[prefixToUserData][event.target.id] > b[prefixToUserData][event.target.id] ){return 1;}
+        return 0;
+      // for email and phone there is no prefix, so just pass in event.target.id
+      }else{
+        if ( a[event.target.id] < b[event.target.id] ){return -1;}
+        if ( a[event.target.id] > b[event.target.id] ){return 1;}
+        return 0;
+      }
+    }
+
+    // set the userFilteredData that gets output to the new array
     var newArray = userFilteredData.sort(compare);
     setUserFilteredData([...newArray]);
-
   }
 
   return (
     <div className="App">
+      <h2>User Information</h2>
 
       {/* place for user to input text, on change event calls the onChangeHandler*/}
+      <p>Type in information, and only fields that contain it will be displayed.</p>
       <input
       type="text"
       id="userSortInput"
       onChange = {event => {onChangeHandler(event)}}
       value = {userInputData}
       />
-
-      <h2>User Information</h2>
-      <button onClick={sortAlpha}>Sort!</button>
+      <br /><br />
 
       {/* display the data for only users who pass the filter */}
+      <table>
+        <thead>
+      <tr>
+        <th>Picture</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>Phone Number</th>
+        <th>Age</th>
+      </tr>
+      <tr>
+        <th></th>
+        {/* each button has an id for what will be sorted in the sortAlpha function */}
+        <th><button class="btn btn-secondary btn-sm" onClick={event => {sortAlpha(event)}} id="first">Sort First Name</button></th>
+        <th><button class="btn btn-secondary btn-sm" onClick={event => {sortAlpha(event)}} id="last">Sort Last Name</button></th>
+        <th><button class="btn btn-secondary btn-sm" onClick={event => {sortAlpha(event)}} id="email">Sort Email</button></th>
+        <th><button class="btn btn-secondary btn-sm" onClick={event => {sortAlpha(event)}} id="phone">Sort Phone Number</button></th>
+        <th><button class="btn btn-secondary btn-sm" onClick={event => {sortAlpha(event)}} id="age">Sort Age</button></th>
+      </tr>
+      </thead>
+      <tbody>
       {userFilteredData.map( (user) => {
-        //console.log('MAP HAPPENING', user)
         return(
-        <tr>
+        <tr key={user.email}>
           <td><img src={user.picture.thumbnail} alt="user_thumbnail"></img></td>
           <td>{user.name.first}</td>
           <td>{user.name.last}</td>
@@ -84,7 +122,10 @@ function App() {
           <td>{user.dob.age}</td>
         </tr>
         )
+        
       })}
+      </tbody>
+      </table>
       
     </div>
   );
